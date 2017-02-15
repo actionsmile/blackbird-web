a56e7b3f9ca54d7be5554f55d3aee8f2dbe11665
Aziz Zainutdin <actionsmile@icloud.com>
2017-02-15 14:50:31 +0300

    First vue attempt

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div class="blackbird-background">
+    <div id="app">
+      <div class="title-logo"></div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -17,7 +19,56 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
 }
+
+html body {
+	margin: 0;
+	padding: 0;
+	top: 0;
+	left: 0;
+}
+
+body {
+	background-color: #ccc;
+}
+
+.blackbird-background {
+  background: url(./assets/bg.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  top: 0;
+  bottom: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.title-logo {
+	margin: 50px auto 0;
+	width: 214px;
+	height: 81px;
+	background: url(./assets/header.png) no-repeat;
+}
+
+.github-sources {
+	margin: 40px 0;
+	text-align: center;;
+}
+
+.github-sources  p, a {
+	font-size: 18pt;
+	color:#fff;
+	font-family: Titillium Web;
+	text-transform: uppercase;
+}
+
+.github-sources a {
+	text-decoration: none;
+	margin-left: .1em;
+	border-bottom: 1px solid;
+}
+
 </style>
---
+++ src/components/Info.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="github-sources">
+  	<p><i class="fa fa-github" aria-hidden="true"></i>
+  	<a href="https://github.com/actionsmile/blackbird">{{source}}</a></p>
+  	<p><i class="fa fa-cog fa-spin fa-fw"></i>{{working}}</p>
+  </div>
+</template>
+
+<script src="https://use.fontawesome.com/e7d12e4dd6.js"></script>
+<script>
+export default {
+  name: 'info',
+  data () {
+    return {
+      source: 'Source code',
+      working: 'Still working on it'
+    }
+  }
+}
+</script>
